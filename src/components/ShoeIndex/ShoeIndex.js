637a30ea49e5684f48bf1ab4a8f8ab3342ab986c
Felipe Morán <felipe.moran11@gmail.com>
2021-11-06 20:26:17 +0000

    Page header now responsive

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { WEIGHTS } from '../../constants';
+import {QUERIES, WEIGHTS} from '../../constants';
 
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
@@ -9,37 +9,50 @@ import Spacer from '../Spacer';
 import ShoeSidebar from '../ShoeSidebar';
 import ShoeGrid from '../ShoeGrid';
 
-const ShoeIndex = ({ sortId, setSortId }) => {
-  return (
-    <Wrapper>
-      <MainColumn>
-        <Header>
-          <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
-        </Header>
-        <Spacer size={32} />
-        <ShoeGrid />
-      </MainColumn>
-      <LeftColumn>
+const ShoeBreadcrumbs = () => {
+    return (
         <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
+            <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+            <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+            <Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
         </Breadcrumbs>
-        <Spacer size={42} />
-        <ShoeSidebar />
-      </LeftColumn>
-    </Wrapper>
-  );
+    )
+}
+
+const ShoeIndex = ({sortId, setSortId}) => {
+    return (
+        <Wrapper>
+            <MainColumn>
+                <Header>
+                    <div>
+                        <MobileBreadcrumbs>
+                            <ShoeBreadcrumbs/>
+                        </MobileBreadcrumbs>
+                        <Title>Running</Title>
+                    </div>
+                    <SortFilterWrapper>
+                        <Select
+                            label="Sort"
+                            value={sortId}
+                            onChange={(ev) => setSortId(ev.target.value)}
+                        >
+                            <option value="newest">Newest Releases</option>
+                            <option value="price">Price</option>
+                        </Select>
+                    </SortFilterWrapper>
+                </Header>
+                <Spacer size={32}/>
+                <ShoeGrid/>
+            </MainColumn>
+            <LeftColumn>
+                <DesktopBreadcrumbs>
+                    <ShoeBreadcrumbs/>
+                </DesktopBreadcrumbs>
+                <Spacer size={42}/>
+                <ShoeSidebar/>
+            </LeftColumn>
+        </Wrapper>
+    );
 };
 
 const Wrapper = styled.div`
@@ -47,10 +60,19 @@ const Wrapper = styled.div`
   flex-direction: row-reverse;
   align-items: baseline;
   gap: 32px;
+  
+  @media ${QUERIES.tabletAndSmaller} {
+    flex-direction: column-reverse;
+    gap: 0;
+  }
 `;
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
+  
+  @media ${QUERIES.tabletAndSmaller} {
+    display: none;
+  }
 `;
 
 const MainColumn = styled.div`
@@ -61,11 +83,35 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
+  
+  @media ${QUERIES.laptopAndSmaller} {
+    align-items: flex-end;
+  }
 `;
 
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
 `;
+
+const SortFilterWrapper = styled.div`
+  @media ${QUERIES.phoneAndSmaller} {
+    display: none;
+  }
+`
+
+const MobileBreadcrumbs = styled.div`
+  display: none;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    display: revert;
+  }
+`
+const DesktopBreadcrumbs = styled.div`
+  @media ${QUERIES.tabletAndSmaller} {
+    display: none;
+  }
+`
+
 
 export default ShoeIndex;
